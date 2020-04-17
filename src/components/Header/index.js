a9611da9bd9a2859deae2447a002de4a6dbf1589
Marcos R F Rosada <ochild@gmail.com>
2020-04-17 15:44:03 +0100

    Component -> Header Layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Header = () => <Container>React@Celfocus</Container>;
+const Header = () => (
+  <Container>
+    <h1>React@Celfocus</h1>
+    <ul>
+      <li>
+        <a href="">Repos</a>
+      </li>
+      <li>
+        <a href="">Users</a>
+      </li>
+    </ul>
+  </Container>
+);
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,11 +3,34 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 60px;
+  padding: 20px;
   background: #202225;
   color: #b9bbbe;
   box-shadow: 0 2px 20px 5px rgba(0, 0, 0, 0.2);
   font-size: 18px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+
+  h1 {
+    color: #ccc;
+    font-size: 22px;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: #fff;
+    padding-left: 10px;
+    font-size: 15px;
+  }
 `;
